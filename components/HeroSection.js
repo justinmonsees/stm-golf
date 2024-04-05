299749783cd06e6258c513bdff1cf0ac43ecfc98
Justin Monsees <justinmonsees@gmail.com>
2024-04-05 14:16:45 -0400

    updated hero image to be optimized

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -6,6 +6,7 @@ import { Button } from "@material-tailwind/react";
 import { useState, useEffect } from "react";
 import InfoCard from "./InfoCard";
 import Link from "next/link";
+import Image from "next/image";
 import {
   faCalendarDays,
   faClock,
@@ -18,71 +19,77 @@ const HeroSection = ({ initItemInfo, initEventInfo }) => {
   const eventInfo = initEventInfo;
 
   return (
-    <div
-      className=" sm:min-h-screen w-full bg-cover bg-no-repeat bg-center bg-blend-overlay bg-black/50 flex text-center flex-col"
-      style={{
-        backgroundImage: `url(${heroImg.src})`,
-      }}
-    >
-      <div className="mt-[90px] sm:mt-auto mb-[90px] sm:mb-0">
-        <div className="text-white relative mt-7">
-          <p className="font-lobster text-2xl sm:text-3xl">18th Annual</p>
-          <p className="font-noto sm:text-4xl font-light sm:leading-10 text-xl leading-tight">
-            St. Thomas More <br /> Francis S. Midura <br />
-            Golf Outing
-          </p>
+    <section className="h-full relative">
+      <Image
+        src={heroImg}
+        alt="image of golf course"
+        id="hero-img"
+        className="z-[-1] object-cover h-full w-full absolute"
+      />
+      <div className="sm:min-h-screen w-full bg-blend-overlay bg-black/50 flex text-center flex-col">
+        <div className="mt-[90px] sm:mt-auto mb-[90px] sm:mb-0">
+          <div className="text-white relative mt-7">
+            <p className="font-lobster text-2xl sm:text-3xl">18th Annual</p>
+            <p className="font-noto sm:text-4xl font-light sm:leading-10 text-xl leading-tight">
+              St. Thomas More <br /> Francis S. Midura <br />
+              Golf Outing
+            </p>
+          </div>
+
+          <div className="flex flex-col sm:flex-row justify-center gap-4 mt-2 px-10">
+            <Button
+              variant="outlined"
+              color="white"
+              className="text-white border-white"
+            >
+              <Link href={"register"}>Register Now</Link>
+            </Button>
+            <Button
+              variant="outlined"
+              color="white"
+              className="text-white border-white"
+            >
+              <Link href={"donate"}>Become a Sponsor</Link>
+            </Button>
+          </div>
         </div>
 
-        <div className="flex flex-col sm:flex-row justify-center gap-4 mt-2 px-10">
-          <Button
-            variant="outlined"
-            color="white"
-            className="text-white border-white"
-          >
-            <Link href={"register"}>Register Now</Link>
-          </Button>
-          <Button
-            variant="outlined"
-            color="white"
-            className="text-white border-white"
-          >
-            <Link href={"donate"}>Become a Sponsor</Link>
-          </Button>
+        <div className="grid lg:grid-cols-4 sm:grid-cols-2 w-full mt-auto gap-y-3 gap-x-3 p-3">
+          <InfoCard
+            infoIcon={faCalendarDays}
+            infoText={new Date(eventInfo.event_date).toLocaleDateString(
+              "en-us",
+              {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+              }
+            )}
+          ></InfoCard>
+
+          <InfoCard
+            infoIcon={faClock}
+            infoText={`Registration: ${eventInfo.arrival_time}\nShotgun Start: ${eventInfo.start_time}`}
+          ></InfoCard>
+
+          <InfoCard
+            infoIcon={faDollarSign}
+            infoText={itemInfo
+              .map((item) => `$${item.cost} Per ${item.name}`)
+              .join(`\n`)}
+          ></InfoCard>
+
+          <InfoCard
+            infoIcon={faLocationDot}
+            infoText={`${eventInfo.venue_name}\n${eventInfo.venue_address}${
+              eventInfo.venue_address2 ? eventInfo.venue_address2 : ""
+            }\n${eventInfo.venue_city}, ${eventInfo.venue_state} ${
+              eventInfo.venue_zip
+            }`}
+          ></InfoCard>
         </div>
       </div>
-
-      <div className="grid lg:grid-cols-4 sm:grid-cols-2 w-full mt-auto gap-y-3 gap-x-3 p-3">
-        <InfoCard
-          infoIcon={faCalendarDays}
-          infoText={new Date(eventInfo.event_date).toLocaleDateString("en-us", {
-            month: "long",
-            day: "numeric",
-            year: "numeric",
-          })}
-        ></InfoCard>
-
-        <InfoCard
-          infoIcon={faClock}
-          infoText={`Registration: ${eventInfo.arrival_time}\nShotgun Start: ${eventInfo.start_time}`}
-        ></InfoCard>
-
-        <InfoCard
-          infoIcon={faDollarSign}
-          infoText={itemInfo
-            .map((item) => `$${item.cost} Per ${item.name}`)
-            .join(`\n`)}
-        ></InfoCard>
-
-        <InfoCard
-          infoIcon={faLocationDot}
-          infoText={`${eventInfo.venue_name}\n${eventInfo.venue_address}${
-            eventInfo.venue_address2 ? eventInfo.venue_address2 : ""
-          }\n${eventInfo.venue_city}, ${eventInfo.venue_state} ${
-            eventInfo.venue_zip
-          }`}
-        ></InfoCard>
-      </div>
-    </div>
+    </section>
   );
 };
 
