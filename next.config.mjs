cd425b3437aa0657c29571cb3fc7e3f87600bdb2
Justin Monsees <justinmonsees@gmail.com>
2024-04-08 18:48:50 -0400

    Corrected version with optimized home page images7

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    deviceSizes: [110, 140, 300, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+};
 
 export default nextConfig;
